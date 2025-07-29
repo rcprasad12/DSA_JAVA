3c9f2cab38abd27e661bb4ecd6cb300c590663ee
RC Prasad <rcprasad.2000@gmail.com>
2025-07-29 22:15:48 +0530

    Inpute

---
+++ src/ArraysExample.java
@@ -0,0 +1,7 @@
+package Arrays;
+
+public class ArraysExample {
+    public static void main(String[] args) {
+        System.out.println("Hello from Arrays!");
+    }
+}
---
+++ src/Input.java
@@ -0,0 +1,12 @@
+public class Input {
+
+    public static void main(String[] args) {
+            int[] arr = new int[5];
+            arr[0] = 22;
+            arr[1] = 33;
+            arr[2] = 44;
+            arr[3] = 55;
+            arr[4] = 66;
+        System.out.println(arr[3]);
+    }
+}
