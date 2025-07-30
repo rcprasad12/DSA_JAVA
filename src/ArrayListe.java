f8f979adfd4b5ce4f2122ad83cdc6fd631e8031a
RC Prasad <rcprasad.2000@gmail.com>
2025-07-30 19:28:59 +0530

    ArrayList

---
+++ src/ArrayListe.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayListe {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(55);
+        list.add(1234567);
+        list.add(65);
+        list.add(888);
+
+        System.out.println(list);
+
+
+    }
+}
