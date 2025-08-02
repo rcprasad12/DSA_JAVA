e0098b8f88f407e7074f306e6a08f32e6442a298
RC Prasad <rcprasad.2000@gmail.com>
2025-08-03 00:29:10 +0530

    Linear Search

---
+++ src/MaxWealth.java
@@ -0,0 +1,22 @@
+public class MaxWealth {
+
+    public static void main(String[] args) {
+
+    }
+    public int maximumWealth(int[][] accounts){
+        int ans = Integer.MIN_VALUE;
+        for(int person = 0 ; person < accounts.length;person++){
+            int sum = 0;
+            for(int account = 0 ; account < accounts[person].length; account++){
+                sum += accounts[person][account];
+
+                if(sum >ans){
+                    ans =sum;
+                }
+
+            }
+            return sum ;
+        }
+        return ans;
+    }
+}
