0bbc2a0dec0e709e5589d9aa9985b7b72126efce
RC Prasad <rcprasad.2000@gmail.com>
2025-08-06 13:54:25 +0530

    Binary Search

---
+++ src/orderAgnostic.java
@@ -0,0 +1,51 @@
+public class orderAgnostic {
+
+    public static void main(String[] args) {
+
+        int[] arr = {3,9,27,81,229,343,455,678,888,9999};
+        int target = 343;
+        int ans = Orderagonistic(arr,target);
+        System.out.println(ans);
+    }
+
+    static int Orderagonistic(int[] arr , int target){
+        int start = 0 ;
+        int end  = arr.length - 1 ;
+
+        boolean isAsc;
+        if(arr[start]< arr[end]){
+            isAsc = true;
+        }
+        else {
+            isAsc = false;
+        }
+
+        while(start <=end){
+            //find the middle element
+            // (start+end)/2 is not used to find middle element , becoz the range of int might exceed for large numbers in java
+            int mid = start + (end-start)/2;
+
+            if(arr[mid] == target){
+                return mid;
+            }
+            if(isAsc){
+                if(target < arr[mid]){
+                    end = mid-1;
+                }
+                else{
+                    start = mid+1;
+                }
+            }
+            else{
+                if(target > arr[mid]){
+                    end = mid-1;
+                } else if (target < arr[mid]) {
+                    start = mid+1;
+                }
+            }
+
+        }
+        return -1;
+    }
+
+}
